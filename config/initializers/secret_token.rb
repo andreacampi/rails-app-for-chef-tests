76110f1ed994bd41f11fcfcc65dd614a08db3ce6
Andrea Campi <andrea.campi@zephirworks.com>
2012-10-31 22:34:59 +0100

    Add a basic Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAppForChefTests::Application.config.secret_token = '18f4185d4f451031ffea62136fc765d8d3626c74dbde755f9e59ee475d9e79e63cecba41efa1e5aac421de44920b4c6db3a4c1a95cb569bcf542086d24f5a71d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAppForChefTests::Application.config.session_store :cookie_store, key: '_rails-app-for-chef-tests_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsAppForChefTests::Application.config.session_store :active_record_store
